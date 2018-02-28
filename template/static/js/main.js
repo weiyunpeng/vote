1a04f2465142bbc336b7d3971537c07672acb576
wei <wei@MacBook-Air.local>
2018-03-01 03:53:11 +0800

    qietu

---
+++ template/static/js/main.js
@@ -0,0 +1,15 @@
+// $(document).ready(function() {
+//     $('.slogan-list-ul').niceScroll({
+//         touchbehavior: false,// 激活拖拽滚动
+//         cursorcolor: '#fff', // 改变滚动条颜色，使用16进制颜色值
+//         background: "#48b0a3", // 轨道的背景颜色
+//         cursoropacitymin: 1, // 当滚动条是隐藏状态时改变透明度, 值范围 1 到 0
+//         cursoropacitymax: 1, // 当滚动条是显示状态时改变透明度, 值范围 1 到 0
+//         cursorwidth: "5px", // 滚动条的宽度，单位：像素
+//         cursorborderradius: "5px", // 滚动条圆角（像素）
+//         hwacceleration: true, // 激活硬件加速
+//         cursorminheight: 2, // 设置滚动条的最小高度 (像素)
+//         nativeparentscrolling: true, // 检测内容底部便于让父级滚动
+//         preventmultitouchscrolling: true
+//     });
+// });
